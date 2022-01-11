a316f4471760c8f804135e48592023aa2759c446
GiAn <gian.gavins@gmail.com>
2022-01-10 21:55:39 -0500

    v0.1.0 add pages Login

---
+++ src/pages/login/Login.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return(
+        <div className="className">
+            <h1>Login Page</h1>
+        </div>
+    );
+}
+
+export default Login;
