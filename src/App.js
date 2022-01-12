2787d3b8cb7149daf093cda91e4acb39760c0005
GiAn <gian.gavins@gmail.com>
2022-01-11 19:01:03 -0500

    header component squeleton

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 import Login from './pages/login/Login.component'
+import Header from './pages/header/Header.component';
 
 function App() {
   return (
     <Routes>
-      <Route exact path='/' element={<Login/>} />
+      <Route exact path='/' element={<Header/>} />
     </Routes>
   );
 }
