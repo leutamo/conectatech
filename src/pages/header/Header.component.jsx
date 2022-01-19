4f72898ddb29d3af4dbd0810c87f4f5a2380a841
Junior Almeyda <jr.almeyda7@gmail.com>
2022-01-18 19:35:34 -0500

    agrega cambios de identacion

--- src/pages/header/Header.component.jsx
+++ src/pages/header/Header.component.jsx
@@ -1,19 +1,17 @@
 
 const UserHeader = ({name}) => {
-    return(
-        <>
-            <header>
-                <nav>
-                    <ul>
-                        <li className="bg-black text-white">icono</li>
-                        <li>Crear Petición</li>
-                        <li>Peticiones Resueltas</li>
-                        <li>Mis Peticiones</li>
-                        <li>{name}</li>
-                    </ul>
-                </nav>
-            </header>
-        </>
-    );
+  return(
+    <header>
+      <nav>
+        <ul>
+          <li className="bg-black text-white">icono</li>
+          <li>Crear Petición</li>
+          <li>Peticiones Resueltas</li>
+          <li>Mis Peticiones</li>
+          <li>{name}</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 export default UserHeader;
