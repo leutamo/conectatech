61b22f8e44344abb620a743dec38000a49b6e1f0
Junior Almeyda <jr.almeyda7@gmail.com>
2022-01-24 10:21:34 -0500

    refactor: arregla la identacion

--- src/pages/aside/AsideLogo.component.jsx
+++ src/pages/aside/AsideLogo.component.jsx
@@ -4,18 +4,18 @@ import './AsideLogo.css'
 const AsideLogo = ()=> {
   return(
     <aside className='w-1/4'>
-	  <div className='bg-gray-300 h-full h-screen py-40'>
-	    <div className='imgContainer'>
-		  <img src={logo} alt='Conectatech Logo' className='w-4/5 mx-auto'/>
-		</div>
-		<p className='my-10 mx-auto w-4/5 frase'>
-		  Cuanto más esperes en corregir un error, más costoso será arreglarlo
-		</p>
-		<h2 className='my-20 font-black conectatech'>
-		  ConectaTech
-	    </h2>
-	  </div>
-	</aside>
+      <div className='bg-gray-300 h-full h-screen py-40'>
+        <div className='imgContainer'>
+          <img src={logo} alt='Conectatech Logo' className='w-4/5 mx-auto'/>
+        </div>
+        <p className='my-10 mx-auto w-4/5 frase'>
+          Cuanto más esperes en corregir un error, más costoso será arreglarlo
+        </p>
+        <h2 className='my-20 font-black conectatech'>
+          ConectaTech
+        </h2>
+      </div>
+    </aside>
   );
 }
 export default AsideLogo;
