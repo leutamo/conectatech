4f47135efd69a3477b6751f9fd7c18a0336cbdfc
Junior Almeyda <jr.almeyda7@gmail.com>
2022-01-24 03:43:27 -0500

    Feat: agrega componente para el registro de tipo usuario y profesional

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import React, { useState } from 'react'
 
 import Login from './pages/login/Login.component'
 //import UserHeader from './pages/header/Header.component';
-
+import AsideLogo from './pages/aside/AsideLogo.component'
+import Registro from './pages/registro/Registro.component'
+import RegistroProfesional from './pages/registro/profesional/RegistroProfesional.component'
 function App() {
   //const [name, setName] = useState('Junior Almeyda')
   return (
     <Routes>
       <Route exact path='/' element={<Login />} />
+      <Route exact path='/ejemplo' element={<RegistroProfesional />} />
     </Routes>
   );
 }
