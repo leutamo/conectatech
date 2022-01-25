44c968e11dca950e38ff3e23625c2180b37e6dfa
Junior Almeyda <jr.almeyda7@gmail.com>
2022-01-25 02:31:07 -0500

    fix: arregla componente registro de usario y profesional

---
+++ src/pages/registro/IndexRegistro.component.jsx
@@ -0,0 +1,12 @@
+import AsideLogo from '../aside/AsideLogo.component'
+import Registro from './Registro.component'
+
+const IndexRegistro = () => {
+  return(
+    <div className='flex'>
+      <AsideLogo />
+      <Registro />
+    </div>
+  );
+}
+export default IndexRegistro;
---
+++ src/pages/registro/profesional/IndexRegistroProfesional.component.jsx
@@ -0,0 +1,12 @@
+import AsideLogo from '../../aside/AsideLogo.component'
+import RegistroProfesional from './RegistroProfesional.component'
+
+const IndexRegistroProfesional = () => {
+  return (
+    <div className='flex'>
+      <AsideLogo />
+      <RegistroProfesional />
+    </div>
+  );
+}
+export default IndexRegistroProfesional;
